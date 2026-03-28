ee40292698c6f4a5a246a34fb7d94872ec9ee31f
Jawaharbasha J <jawaharbasha2006@gmail.com>
2026-03-28 13:29:37 +0530

    Replace resume with Adarsh Mishra CV and add docx support

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/Specialised CV priyanshuPDF.pdf'
+import Resume from '../../assets/Adarsh_Mishra_CV.docx'
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
